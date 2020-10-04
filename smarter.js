c39f8a491187e71b0954dadffe3855cd86481910
kalevnakah <calebschmidt@gmail.com>
2020-10-03 23:06:55 -0500

    Chapter: 10 complete.

--- smarter.js
+++ smarter.js
@@ -1,1 +1,10 @@
-alert("Hi There!");
+var greeting = "Hello World!"
+var someNum = 42;
+someNum = ++someNum + 3; // 46
+someNum = ((someNum + 7) * 7) + 1; // (53*7) = 371 + 372
+alert(greeting + " " + someNum); //372
+var answer1 = prompt("What is your number?", "42");
+if (answer1 == someNum) {
+    var myNumber = someNum;
+    alert("That's my number too!")
+}
