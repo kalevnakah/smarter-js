3ce1afb609223a15c1aacac6d0b600838b950774
kalevnakah <calebschmidt@gmail.com>
2020-10-04 13:28:43 -0500

    Chapter 1: Complete

--- smarter.js
+++ smarter.js
@@ -4,9 +4,13 @@ someNum = ++someNum + 3; // 46
 someNum = ((someNum + 7) * 7) + 1; // (53*7) = 371 + 372
 alert(greeting + " " + someNum); //372
 var answer1 = prompt("What is your number?", "42");
+var numbers = [answer1 ,someNum, "42"];
 if (answer1 == someNum) {
     var myNumber = someNum;
-    alert("That's my number too!")
+    alert("That's my number too!" + " " + numbers[1]);
+}
+else if (answer1 === numbers[2]) {
+    alert("What is the meaning of life?");
 }
 else {
     alert("I don't like that number!");
